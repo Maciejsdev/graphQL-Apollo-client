e0ab9edd4871e3ce63539016265d9265a25ebfdb
Maciek <maciejsdev@gmail.com>
2025-02-04 13:16:36 +0100

    git hub graphql, components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-const App = () => {
+import { useState } from "react";
+import SearchForm from "./components/form/SearchForm";
+import UserProfile from "./components/user/UserProfile";
+
+function App() {
+  const [userName, setUserName] = useState("Maciejsdev");
   return (
-    <h1 className='text-3xl font-bold p-8'>Search Github Users - Starter</h1>
+    <main className="mx-auto max-w-6xl px-8 py-20">
+      <SearchForm userName={userName} setUserName={setUserName} />
+      <UserProfile userName={userName} />
+    </main>
   );
-};
+}
 export default App;
